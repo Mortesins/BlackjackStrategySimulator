06d29f8192bf4883552e59519daf0c53202fc9ea
MathemagicalCalibrations <robinb299@gmail.com>
2017-06-27 15:43:55 +0200

    No, Daddy, I don't want you to buy me a hat!

--- Shoe.cpp
+++ Shoe.cpp
@@ -1,1 +1,39 @@
-#include "${a}.hpp"
+#include "Shoe.hpp"
+
+Shoe::Shoe(unsigned short numberOfDecks, double penetration)
+{
+    this->numberOfDecks = numberOfDecks;
+    this->unused = 52 * numberOfDecks * (1-penetration);
+    shuffle();
+}
+
+void Shoe::shuffle()
+{
+    unsigned short tmp[13];
+    for (unsigned short i=0; i<13; ++i)
+        tmp[i] = 4 * numberOfDecks;
+    srand(time(0));
+    unsigned short r;
+    for (unsigned short i=0; i < 52*numberOfDecks; ++i)
+    {
+        r = rand() % 13;
+        if (tmp[r] > 0)
+        {
+            cards.push_back(((r+1) % 10) * (1 - (r+1) / 10) + ((r+1) / 10) * 10);
+            tmp[r]--;
+        }
+        else{i--;}
+    }
+}
+
+unsigned short Shoe::getCard()
+{
+    unsigned short card=cards[cards.size()];
+    cards.pop_back();
+    return card;
+}
+
+bool Shoe::isFinished()
+{
+    return cards.size() > unused;
+}
--- Shoe.hpp
+++ Shoe.hpp
@@ -1,7 +1,26 @@
 #ifndef SHOE_HPP
 #define SHOE_HPP
 
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <string>
 
+using namespace std;
+
+class Shoe
+{
+  private:
+    unsigned short unused;
+    unsigned short numberOfDecks;
+    vector <unsigned short> cards;
+
+  public:
+    Shoe(unsigned short numberOfDecks, double penetration);
+    void shuffle();
+    unsigned short getCard();
+    bool isFinished();
+};
 
 
 
