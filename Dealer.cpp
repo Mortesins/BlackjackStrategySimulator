b760ad38496419315e49ba11c911221ef56be6b2
MathemagicalCalibrations <robinb299@gmail.com>
2017-07-11 17:08:47 +0200

    Added Dealer, except for play()

--- Dealer.cpp
+++ Dealer.cpp
@@ -1,1 +1,30 @@
-#include "${a}.hpp"
+#include "Dealer.hpp"
+
+void Dealer::updateHand()
+{
+    hand = 0;
+    bool ace;
+    for(unsigned short i = 0; i <= cards.size(); i++)
+    {
+        hand += cards[i];
+        if (cards[i] == 0)
+            ace = true;
+    }
+    if (hand <= 11 && ace == true)
+        soft = true;
+}
+
+unsigned short Dealer::getHand()
+{
+    return hand;
+}
+
+unsigned short Dealer::upCard()
+{
+    return cards[0];
+}
+
+void Dealer::hit(unsigned short card)
+{
+    cards.push_back(card);
+}
--- Dealer.hpp
+++ Dealer.hpp
@@ -1,7 +1,24 @@
 #ifndef DEALER_HPP
 #define DEALER_HPP
 
+#include <vector>
 
+using namespace std;
+
+class Dealer
+{
+  private:
+    vector <unsigned short> cards;
+    unsigned short hand;
+    bool soft;
+    void updateHand();
+
+  public:
+    unsigned short getHand();
+    unsigned short upCard();
+    void play();
+    void hit(unsigned short h);
+};
 
 
 
